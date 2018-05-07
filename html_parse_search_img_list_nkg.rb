4a620acb86f4b1b90bde469a4e77c87fd3324810
MeowMya <Evina666@gmail.com>
2018-05-08 01:06:40 +0500

    #

--- html_parse_search_img_list_nkg.rb
+++ html_parse_search_img_list_nkg.rb
@@ -5,9 +5,20 @@ require 'open-uri'
 require 'nokogiri'
 
 
-html_text = open('https://wallpaper.mob.org.ru/')
-# html_text = html_text.read()
+url = 'https://wallpaper.mob.org.ru/'
+
+html_text = open(url)
 doc = Nokogiri::HTML(html_text)
+
+url_list = []
+
 doc.css('.picture-list .bg-image > img').each do |img|
-  puts img['src'] + ' ' + img['title']
+  # puts img['src'] + ' ' + img['title']
+  url_img = img['src']
+  url_img = URI::join(url, url_img).to_s
+  p url_img
+
+  url_list << url_img
 end
+
+p url_list
